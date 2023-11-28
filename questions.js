564624a49e08b556262b2f669c47eb54eb2667f3
Adrian Randoll <randolla66@gmail.com>
2023-11-27 16:10:19 -0800

    Added the questions that inquirer will use to prompt the user

---
+++ questions.js
@@ -0,0 +1,19 @@
+module.exports = {
+    questions: [
+        {
+            type:'list',
+            name:'Directory',
+            message:'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'Veiw All Departments',
+                'Add Departmnets'
+            ]
+        },
+
+    ]
+};
